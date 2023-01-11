86ae586f3172de60b1cceb12d42886d6cefdf9ae
SandeshGC <gcsandesh01@gmail.com>
2023-01-11 13:58:12 +0545

    display users

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -3,17 +3,22 @@ import React, { useEffect, useState } from "react";
 export default function Users() {
 	const [users, setUsers] = useState([]);
 	useEffect(() => {
-		let allUsers = async () =>
-			await fetch("http://127.0.0.1:9988/api/users").then((res) => res.json());
-		setUsers(allUsers);
+		fetch("http://127.0.0.1:9988/api/users")
+			.then((res) => res.json())
+			.then((data) => setUsers(data));
 	}, []);
-	// const usersList = users.map((eachUser) => <li>{eachUser.name}</li>);
 
-	// useEffect(async () => {});
+	const usersList = users.map((eachUser) => (
+		<li key={eachUser.user_id}>{eachUser.name}</li>
+	));
+
 	return (
 		<div>
-			{/* {usersList} */}
-			This page is for admin purpose. police and admin
+			This page is for admin purpose only.
+			<div>
+				All Users:
+				<ul className="indent-4">{usersList}</ul>
+			</div>
 		</div>
 	);
 }
