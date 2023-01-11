3dd8f2105a15f5635393630d5b35b50e4550f4f8
SandeshGC <gcsandesh01@gmail.com>
2023-01-11 13:11:54 +0545

    change user to record, install react-icons, add routes, modify pages

---
+++ client/src/components/UserPreview.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function UserPreview() {
+	return (
+		<div>
+			<p>photo</p>
+			<p>name</p>
+			<p>age</p>
+			<p>gender</p>
+			<p>crime</p>
+		</div>
+	);
+}
---
+++ client/src/pages/AddRecord.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AddRecord() {
+	return <div>Form to create new Record</div>;
+}
