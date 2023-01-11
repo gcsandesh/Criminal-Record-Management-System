f453aeb47d99c22cfaae60a018b658c1a8e50258
SandeshGC <gcsandesh01@gmail.com>
2023-01-11 14:59:57 +0545

    format file

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -3,10 +3,10 @@ module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
-      fontFamily:{
-        Manrope: ["Manrope", "sans-serif"]
-      },
-    },
+			fontFamily: {
+				Manrope: ["Manrope", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
