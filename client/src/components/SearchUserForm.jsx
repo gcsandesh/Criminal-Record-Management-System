a3985c1d76decd89d87698842ae1b3a0b6d957cc
SandeshGC <gcsandesh01@gmail.com>
2023-01-12 21:46:17 +0545

    change style of search form

--- client/src/components/SearchUserForm.jsx
+++ client/src/components/SearchUserForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 export default function SearchUserForm({
 	formData,
@@ -16,39 +17,37 @@ export default function SearchUserForm({
 			<form
 				method="GET"
 				onSubmit={handleSubmit}
-				className="flex flex-col justify-between items-start rounded-md gap-2 p-4 bg-light text-dark"
+				className="flex justify-between items-center rounded-md gap-2 p-4 bg-light text-dark"
 			>
-				<div className="flex justify-between gap-2">
-					<div className="flex flex-col items-start gap-1 justify-between w-full my-2">
-						<label htmlFor="firstName">First Name</label>
-						<input
-							onChange={handleInput}
-							className="bg-gray-200 rounded-sm px-2 w-36"
-							type={"text"}
-							name="firstName"
-							value={formData.firstName}
-						/>
-					</div>
-					<div className="flex flex-col items-start gap-1 justify-between w-full my-2">
-						<label htmlFor="middleName">Middle Name</label>
-						<input
-							onChange={handleInput}
-							className="bg-gray-200 rounded-sm px-2 w-36"
-							type={"text"}
-							name="middleName"
-							value={formData.middleName}
-						/>
-					</div>
-					<div className="flex flex-col items-start gap-1 justify-between w-full my-2">
-						<label htmlFor="lastName">Last Name</label>
-						<input
-							onChange={handleInput}
-							className="bg-gray-200 rounded-sm px-2 w-36"
-							type={"text"}
-							name="lastName"
-							value={formData.lastName}
-						/>
-					</div>
+				<div className="flex flex-col items-start gap-1 justify-between w-full my-2">
+					<label htmlFor="firstName">First Name</label>
+					<input
+						onChange={handleInput}
+						className="bg-gray-200 rounded-sm px-2 w-36"
+						type={"text"}
+						name="firstName"
+						value={formData.firstName}
+					/>
+				</div>
+				<div className="flex flex-col items-start gap-1 justify-between w-full my-2">
+					<label htmlFor="middleName">Middle Name</label>
+					<input
+						onChange={handleInput}
+						className="bg-gray-200 rounded-sm px-2 w-36"
+						type={"text"}
+						name="middleName"
+						value={formData.middleName}
+					/>
+				</div>
+				<div className="flex flex-col items-start gap-1 justify-between w-full my-2">
+					<label htmlFor="lastName">Last Name</label>
+					<input
+						onChange={handleInput}
+						className="bg-gray-200 rounded-sm px-2 w-36"
+						type={"text"}
+						name="lastName"
+						value={formData.lastName}
+					/>
 				</div>
 				<div className="flex justify-between gap-2">
 					<div className="flex flex-col items-start gap-1 justify-between w-full my-2">
@@ -83,11 +82,10 @@ export default function SearchUserForm({
 					/>
 				</div>
 				<button
-					// onClick={handleSubmit}
-					className="bg-green-500 text-white rounded-full w-1/2 mx-auto text-sm p-1"
+					className="bg-green-600 h-full justify-between flex items-center gap-2 text-white rounded-full w-1/2 mx-auto text-sm px-4 p-1"
 					type="submit"
 				>
-					Search
+					<span>Search</span> <FaSearch />
 				</button>
 			</form>
 		</div>
