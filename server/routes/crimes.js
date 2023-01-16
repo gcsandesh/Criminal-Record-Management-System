b13e81411b8a30b3bc466f25e1e2d44a557c3ec6
SandeshGC <gcsandesh01@gmail.com>
2023-01-16 22:17:33 +0545

    add routes to get crimes and add crimes

--- server/routes/crimes.js
+++ server/routes/crimes.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
+const db = require("../config/db");
 
+// *********** get all crimes *********/
 router.get("/", (req, res) => {
-	res.send("all crimes");
+	db.query("SELECT * FROM crimes", (error, result) => {
+		if (error) return res.status(404).send("No items found!");
+		return res.send(result);
+	});
+	res.send(crimes);
+});
+
+// *********** add a crime ************/
+router.post("/create", (req, res) => {
+	const crimeName = req.body.name;
+	db.query("INSERT INTO crimes(name) VALUES(?)", crimeName, (error, result) => {
+		if (error) return res.status(400).send("Error inserting data!");
+		return res.send(result);
+	});
 });
 
 module.exports = router;
