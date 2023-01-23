f9e9fc2573dd6d3664953a3d4c6258ede39384af
SandeshGC <gcsandesh01@gmail.com>
2023-01-23 16:55:27 +0545

    add route to delete record, improve error handling

--- server/routes/Records/deleteRecord.js
+++ server/routes/Records/deleteRecord.js
@@ -3,12 +3,27 @@ const db = require("../../config/db")
 
 router.delete("/id/:id", (req, res) => {
 	const recordId = req.params.id
+	const result = []
+	// first find item
 	db.query(
-		"DELETE * FROM records WHERE record_id=?",
+		"SELECT * FROM records WHERE record_id=?",
 		recordId,
 		(error, result) => {
-			if (error) res.status(404).send("Couldnot find record with given id!")
-			res.send(result)
+			if (error) res.status(500).send(error)
+			else if (!result.length) res.status(404).send("No record found!")
+			else {
+				// if no error in searching item, and if item is found, then delete item
+				db.query(
+					"DELETE FROM records WHERE record_id=?",
+					recordId,
+					(error, result) => {
+						// if there is error while deleting record
+						if (error) res.status(500).send(error)
+					}
+				)
+				// if record existed and is now deleted, then send deleted record
+				res.send(result)
+			}
 		}
 	)
 })
