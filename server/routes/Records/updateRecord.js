f583f781a9d5c6d9c0b5cea109045ab54d6e0cac
SandeshGC <gcsandesh01@gmail.com>
2023-01-23 16:30:18 +0545

    fix table name error in query

--- server/routes/Records/updateRecord.js
+++ server/routes/Records/updateRecord.js
@@ -4,30 +4,31 @@ const db = require("../../config/db")
 router.patch("/id/:id", (req, res) => {
 	const recordId = req.params.id
 	const {
-		first_name,
-		middle_name,
-		last_name,
+		firstName,
+		middleName,
+		lastName,
 		age,
-		gender_id,
-		height_inch,
-		crime_id,
+		genderId,
+		height,
+		crimeId,
 		photo,
 	} = req.body
 	db.query(
-		"UPDATE crimes SET first_name=?, middle_name=?, last_name=?, age=?, gender_id=?, height_inch=?, crime_id=?, photo=? WHERE record_id=?",
+		"UPDATE records SET first_name=?, middle_name=?, last_name=?, age=?, gender_id=?, height_inch=?, crime_id=?, photo=? WHERE record_id=?",
 		[
-			first_name,
-			middle_name,
-			last_name,
+			firstName,
+			middleName,
+			lastName,
 			age,
-			gender_id,
-			height_inch,
-			crime_id,
+			genderId,
+			height,
+			crimeId,
 			photo,
 			recordId,
 		],
 		(error, result) => {
-			if (error) res.status(404).send("Record with given id was not found!")
+			// if (error) res.status(404).send("Record with given id was not found!")
+			if (error) res.status(400).send(error)
 			res.send(result)
 		}
 	)
