f947355e3150a828ca4bd44b37c15834f8f460db
SandeshGC <gcsandesh01@gmail.com>
2023-01-29 23:57:00 +0545

    add upload photo feature, display photos from backend properly
    Date:      Tue Jan 29 23:55:10 2023 +0545

--- server/routes/Crimes/getCrime.js
+++ server/routes/Crimes/getCrime.js
@@ -4,7 +4,8 @@ const db = require("../../config/db")
 // *********** get all crimes *********/
 router.get("/", (req, res) => {
 	db.query("SELECT * FROM crimes", (error, result) => {
-		if (error) return res.status(404).send("No items found!")
+		if (error) return res.status(500).send(error)
+		if (!result.length) return res.status(404).send(result)
 		return res.send(result)
 	})
 })
@@ -15,8 +16,9 @@ router.get("/id/:id", (req, res) => {
 		"SELECT * FROM crimes WHERE crime_id=?",
 		crimeId,
 		(error, result) => {
-			if (error) res.status(404).send("No crime found with given id!")
-			res.send(result)
+			if (error) return res.status(500).send(error)
+			if (!result.length) return res.status(404).send(result)
+			return res.send(result)
 		}
 	)
 })
