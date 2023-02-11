8e62eeb87bf9ed753c7d8c0c0517ae55d51e90de
SandeshGC <gcsandesh01@gmail.com>
2023-02-11 18:17:20 +0545

    fix stats page for admin

--- client/src/components/Admin/Stats.jsx
+++ client/src/components/Admin/Stats.jsx
@@ -1,63 +1,57 @@
 import React, { useEffect, useState } from "react"
 
 export default function Stats() {
-	const [records, setRecords] = useState([])
-	const [users, setUsers] = useState([])
+    const [records, setRecords] = useState([])
+    const [users, setUsers] = useState([])
 
-	const noOfRecords = records.length
-	const noOfUsers = users.length
-	const noOfCriminals = records.filter((record) => record.isCriminal).length
-	const noOfNonCriminals = records.filter((record) => !record.isCriminal).length
-	//count occurence of crime in every record
-	const mostCommittedCrime = getMostCommittedCrime()
-	function getMostCommittedCrime() {
-		const committedCrimes = records.map((record) =>
-			record.crime_id ? record.crime_id : 0
-		)
-		const counts = [] //gives counts of every crime
+    const noOfUsers = users.length
+    const noOfRecords = records.length
+    const noOfCriminals = records.filter(
+        (record) => record.crime_id !== 0
+    ).length
+    const noOfNonCriminals = records.filter((record) => !record.crime_id).length
 
-		committedCrimes.forEach((value) => {
-			return counts.push(
-				committedCrimes.filter(
-					(eachCrimeNum) => eachCrimeNum && eachCrimeNum == value
-				).length
-			)
-		})
-		const maxCount = Math.max.apply(Math, counts)
-		const maxCountLoc = counts.indexOf(maxCount)
-		const maxCommittedCrimeId = committedCrimes[maxCountLoc]
+    //count occurence of crime in every record
+    const mostCommittedCrime = getMostCommittedCrime()
+    function getMostCommittedCrime() {
+        const committedCrimes = records.map((record) => record.cname)
+        const counts = [] //gives counts of every crime
 
-		if (maxCommittedCrimeId == 21) {
-			return "Terrorism"
-		} else if (maxCommittedCrimeId == 0) {
-			return "None"
-		} else if (maxCommittedCrimeId == 2) {
-			return "Murder"
-		}
-	}
-	// now get all crimes and match the crimeWithMaxCount to crime_id in crimes table
-	useEffect(() => {
-		fetch("http://localhost:9988/api/records/get")
-			.then((res) => res.json())
-			.then((data) => setRecords(data))
+        committedCrimes.forEach((value) => {
+            return counts.push(
+                committedCrimes.filter(
+                    (crime) => crime != "-" && crime == value
+                ).length
+            )
+        })
+        const maxCount = Math.max.apply(Math, counts)
+        const maxCountLoc = counts.indexOf(maxCount)
+        const maxCommittedCrime = committedCrimes[maxCountLoc]
+        return maxCommittedCrime
+    }
 
-		fetch("http://localhost:9988/api/users/get")
-			.then((res) => res.json())
-			.then((data) => setUsers(data))
-	}, [])
+    useEffect(() => {
+        fetch("http://localhost:9988/api/records/get")
+            .then((res) => res.json())
+            .then((data) => setRecords(data))
 
-	return (
-		<div>
-			<h2 className="font-bold text-lg underline underline-offset-4">
-				Site Statistics
-			</h2>
-			<ul>
-				<li>Users: {noOfUsers}</li>
-				<li>Records: {noOfRecords}</li>
-				<li>Criminals: {noOfCriminals}</li>
-				<li>Non-criminals: {noOfNonCriminals}</li>
-				<li>Most committed crime: {mostCommittedCrime}</li>
-			</ul>
-		</div>
-	)
+        fetch("http://localhost:9988/api/users/get")
+            .then((res) => res.json())
+            .then((data) => setUsers(data))
+    }, [])
+
+    return (
+        <div>
+            <h2 className="font-bold text-lg underline underline-offset-4">
+                Site Statistics
+            </h2>
+            <ul>
+                <li>Users: {noOfUsers}</li>
+                <li>Records: {noOfRecords}</li>
+                <li>Criminals: {noOfCriminals}</li>
+                <li>Non-criminals: {noOfNonCriminals}</li>
+                <li>Most committed crime: {mostCommittedCrime}</li>
+            </ul>
+        </div>
+    )
 }
